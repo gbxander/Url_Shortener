b98a3153f39cf148e991151d4603d72a4209a1e7
gbxander <gbxander14@gmail.com>
2021-12-10 05:18:23 -0800

    update url record validators

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,12 +2,12 @@ require 'uri'
 
 class Url < ApplicationRecord
     validates_presence_of :original_url
-    validates :url, format: URI::regexp(["http", "https"])
-    validates :url_alias, uniqueness: { case_sensitive: false, message: "The alias -- %{value} -- already exists!" }
+    validates :url_slug, uniqueness: { case_sensitive: false, message: "'%{value}'already exists!" }
     validates_presence_of :created_by
     validates_presence_of :url_expiration
     validates_presence_of :url_usage
+    validates_presence_of :short_url
     validates_length_of :original_url, within: 5..250, message: "too long"
-    validates_length_of :short_url, :maximum => 20, message: "too long"
+    validates_length_of :url_slug, :maximum => 20, message: "too long"
     validates_length_of :created_by, :maximum => 50
 end
