6551526ea3b63fec5648bf8a6f760024d49f2de0
gbxander <gbxander14@gmail.com>
2021-12-10 13:24:37 -0800

    add url env var to development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.url = "www.miniurl.com"
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
