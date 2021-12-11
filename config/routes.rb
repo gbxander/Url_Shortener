d193eb3103a9007857028f9f9172ca6d1604b78e
gbxander <gbxander14@gmail.com>
2021-12-10 20:46:27 -0800

    add route for displaying shortened url

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :urls, only: [:index, :create, :destroy, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'urls/:url_slug' => 'urls#redirect'
+  get 'urls/show/:id' => 'urls#show'
 end
